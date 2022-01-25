e974004bdef4dd3e498481a4d9bb34454d935872
leo94-chen <hbxtck@gmail.com>
2022-01-25 08:59:07 -0800

    evaluation

--- eval.sh
+++ eval.sh
@@ -7,7 +7,6 @@ export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
 python evaluate.py --data real_test\
-  --model model/256_model_15.pth\
-  --result_dir ./evaluate_result/hkclr01/\
+  --model model/model.pth\
   --num_structure_points 256\
   --result_dir results/real
